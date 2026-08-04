bbcf2b420d0ce7ed534375b0c6603a60d61886db
nevskie <funaneva@filamer.edu.ph>
2026-08-04 17:20:18 +0800

    change nav

--- freen.js
+++ freen.js
@@ -200,23 +200,21 @@ topBtn.addEventListener("click",()=>{
    Smooth Navigation
 ========================== */
 
-document.querySelectorAll("nav a")
-.forEach(link=>{
+document.querySelectorAll('nav a').forEach(anchor => {
 
-    link.addEventListener("click",function(e){
+    anchor.addEventListener('click', function (e) {
 
         e.preventDefault();
 
-        const target =
-        document.querySelector(
-        this.getAttribute("href")
-        );
+        const target = document.querySelector(this.getAttribute('href'));
 
-        target.scrollIntoView({
+        if(target){
 
-            behavior:"smooth"
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
 
-        });
+        }
 
     });
 
